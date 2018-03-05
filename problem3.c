015fd81a47b28ceb343e8645f6a78f1c62820b09
thegrassisstillgreen <andygrass92@gmail.com>
2018-03-05 12:41:14 -0500

    Add Solutions for Problem 3
    problem3.c is the first attempt at the problem and problem3Alternate.c is a refined method that works a bit quicker. The former increments through all numbers (less than the target), whereas the latter uses the tree method for factoring and reduces the amount of computation by reassigning the target number as a factor when a factor is found. Using the fact that a factors of a number's factor are also factors of that original number, the number of iterations is drastically reduced as factors are found.

---
+++ problem3.c
@@ -0,0 +1,49 @@
+/********************************************************************************
+*	Solution to Problem 3 (www.projecteuler.net/problem=3)			*
+*	Author: Andrew Grass							*
+*	Date: March 4, 2018							*
+*										*
+*	Original problem:							*
+*	What is the largest prime factor of the number 600851475143?		*
+*										*
+*	!!NOTE!! to compile, you need to include -lm at the end of gcc		*
+*										*
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+#define TARGETNUMBER  600851475143
+
+/*
+Count down from the squareroot of the target and check if any of the numbers
+are a factor of the target. If so, then check if the squareroot of that number
+has any factors. If not, then that number is your largest prime factor. */
+
+long long int LargestPrimeFactor() {
+
+	long long int i, j;
+	long long int maxPrime;
+
+	for (i = sqrt(TARGETNUMBER); i > 1; i--) {
+		if (TARGETNUMBER % i == 0) {
+			maxPrime = i;
+			for (j = sqrt(maxPrime); j > 0; j--) {
+				if (maxPrime % j == 0) {
+					break;
+				} else if (j == 2) {
+					return maxPrime;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main () {
+
+	printf("The largest prime factor of %lli is %lli.\n", TARGETNUMBER, LargestPrimeFactor());
+
+	return 0;
+}
---
+++ problem3Alternate.c
@@ -0,0 +1,70 @@
+/********************************************************************************
+*	Solution to Problem 3 (www.projecteuler.net/problem=3			*
+*	Author: Andrew Grass							*
+*	Date: March 4, 2018							*
+*										*
+*	Original problem:							*
+*	What is the largest prime factor of the number 600851475143?		*
+*										*
+*	!!NOTE!! When compiling, you must include -lm at the end of gcc,	*
+*	otherwise math library will not be used and sqrt() will not work.	*
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+#define TARGETNUMBER 600851475143
+
+/*
+divide the target by incrementally larger numbers until get a factor.
+re-assign the factor as the "new" target since every factor of the
+new target will also be a factor of the original target.
+note that the for loop runs until reaching half of the factor, which
+decreases which each new found factor. */
+long long int TreeMethod() {
+
+	long long int i;
+	long long int factor = TARGETNUMBER;
+
+	for (i = 1; i <= (factor / 2) + 1; i++) {
+		if ((factor % i == 0) && (i > 1)) {
+			factor /= i;
+		}
+	}
+
+	return factor;
+}
+
+/*
+to confirm TreeMethod(), increment from 1 to the squareroot
+of the result. If the quotient of the result and any of these
+incrementing numbers does not produce a remainder, the number
+therefore cannot be prime. */
+int CheckPrime(long long int factor) {
+
+	long long int i;
+
+	for (i = 0; i < sqrt(factor); i++) {
+		if (i > 1 && factor % i == 0) {
+			return 0; // where 0 is false
+		}
+	}
+
+	return 1; // where 1 is true
+}
+
+int main () {
+
+	long long int potentialFactor;
+	int primality = 0;
+
+	potentialFactor = TreeMethod();
+	primality = CheckPrime(potentialFactor);
+
+	if (primality == 1) {
+		printf("The largest prime factor of the number %lli is %lli.\n", TARGETNUMBER, potentialFactor);
+	} else {
+		printf("Could not find a prime factor for %lli.\n", TARGETNUMBER);
+	}
+	return 0;
+}
